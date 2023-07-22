a42416cf8ed1115d0b7f5744137b9cdebf24f23b
samartined <70282660+samartined@users.noreply.github.com>
2023-07-22 17:59:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from operaciones import *
+
+sumar(5, 5)
+resta(5, 5)
+multiplicar(5,5)
+dividir(5,5)
---
+++ operaciones.py
@@ -0,0 +1,16 @@
+
+def sumar(n1,n2):
+    suma = n1+n2
+    print(suma)
+
+def resta(n1,n2):
+    resta = n1-n2
+    print(resta)
+
+def multiplicar(n1,n2):
+    mult = n1*n2
+    print(mult)
+
+def dividir(n1,n2):
+    divid = n1/n2
+    print(divid)
