92fed8fa1b5be00646144617108ff11d2c0888c4
samartined <70282660+samartined@users.noreply.github.com>
2023-07-22 12:02:55 +0200

    Create Calculadora IMC.py

---
+++ Calculadora IMC.py
@@ -0,0 +1,8 @@
+peso = float(input("Ingresa tu peso en kg: "))
+estatura = float(input("Ingresa tu estatura en metros: "))
+
+imc = peso / estatura ** 2
+
+imc_redondeado = round(imc, 2)
+
+print("Tu índice de masa corporal es:", imc_redondeado)
